ede31ea7dc0d5470145cefdb99e9193c6c83efd0
dilshan.d3 <dilshan72404@gmail.com>
2024-11-10 13:09:25 +0530

    backend production check up

--- PortFolio-Backend/src/app.js
+++ PortFolio-Backend/src/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json({limit : "16kb"}));
 app.use(express.urlencoded({extended : true}))
 
-app.get("/home",(req,res) => {
+app.get("/playground",(req,res) => {
     res.send("i am running on port ")
 })
 
