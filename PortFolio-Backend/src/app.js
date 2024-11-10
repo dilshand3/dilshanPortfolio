ae962c85ad90d6f6e987a8efbd6a774200ec33bf
dilshan.d3 <dilshan72404@gmail.com>
2024-11-10 13:31:32 +0530

    origin backen default

--- PortFolio-Backend/src/app.js
+++ PortFolio-Backend/src/app.js
@@ -8,9 +8,8 @@ dotenv.config({
 const app = express();
 
 app.use(cors({
-    // origin : process.env.CORS_ORIGIN,
-    // credentials : true
-    origin: "*"
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
 }))
 
 app.use(cookieParser());
