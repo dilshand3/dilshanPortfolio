f5b5eb065a395ed4a79cdb52b339d14544822c71
dilshan.d3 <dilshan72404@gmail.com>
2024-11-10 13:28:18 +0530

    allow cors origin from everywhere

--- PortFolio-Backend/src/app.js
+++ PortFolio-Backend/src/app.js
@@ -8,8 +8,9 @@ dotenv.config({
 const app = express();
 
 app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
+    // origin : process.env.CORS_ORIGIN,
+    // credentials : true
+    origin: "*"
 }))
 
 app.use(cookieParser());
